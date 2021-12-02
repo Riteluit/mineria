77f5d9e240bf8f6651a82faa7226c240aa7322b2
Riteluit <camiloarmandorojas@gmail.com>
2021-12-01 20:41:52 -0500

    new commit

--- main.py
+++ main.py
@@ -3,28 +3,6 @@ import  requests as rq
 import json
 import mysql.connector
 
-def get_tendencias():
-    trends = json.loads(rq.get('https://api.mercadolibre.com/trends/MCO').content)
-    for trend in trends:
-        searches = json.loads(rq.get(f"https://api.mercadolibre.com/sites/MCO/search?q={trend['keyword'].replace(' ','%20')}").content)
-        #print(searches[])
-        for search in searches['results']:
-            results = search['id'].strip() 
-            #print(results)
-            items = json.loads(rq.get(f"https://api.mercadolibre.com/items/{results}").content)
-            #print(results, items['catalog_product_id'])
-            item_id = items['catalog_product_id']
-            if not item_id == None: 
-                reviews = json.loads(rq.get(f"https://api.mercadolibre.com/reviews/item/{item_id}").content)
-                #print(rewis)
-                if reviews['rating_average'] > 0.0:
-                    #print(reviews['reviews'])
-                    for review in reviews['reviews']:
-                        #print(review['content'])
-                        mysql_insertArticulos(item_id,items['title'],items['price'],reviews['rating_average'])
-                        mysql_insertReview(review['id'],review['title'],review['content'],reviews['rating_average'],review['date_created'],item_id)
-
-
 def mysql_insertReview(id,title,content,rating,date,item_id):
     #Open Connection
     conexion1 = mysql.connector.connect(host="localhost", user="root", passwd="",database="mineria")
@@ -46,8 +24,7 @@ def mysql_insertReview(id,title,content,rating,date,item_id):
     
     conexion1.close() 
 
-
-def mysql_insertArticulos(id,title,price,rating_average):
+def mysql_insertArticulos(id,title,price,rating_average,num_review, fut_tend):
     #Open Connection
     conexion1 = mysql.connector.connect(host="localhost", user="root", passwd="",database="mineria")
     cursor1 = conexion1.cursor()
@@ -60,11 +37,42 @@ def mysql_insertArticulos(id,title,price,rating_average):
     #print(art)
     if (len(art) == 0 ):
         #print("HOLA ENTRE!!!")
-        insert = "insert into articulos(id, nombre_categoria, precio_avg, rating_avg) values (%s,%s,%s,%s)"
-        datos = (id, title, price, rating_average,)       
+        insert = "insert into articulos(id, nombre_categoria, precio_avg, rating_avg, num_reviews, fut_tend) values (%s,%s,%s,%s,%s,%s)"
+        datos = (id, title, price, rating_average,num_review,fut_tend,)       
         cursor1.execute(insert, datos)
         conexion1.commit()
     conexion1.close()
-         
+                 
+
+def get_tendencias():
+    trends = json.loads(rq.get('https://api.mercadolibre.com/trends/MCO').content)
+    for trend in trends:
+        searches = json.loads(rq.get(f"https://api.mercadolibre.com/sites/MCO/search?q={trend['keyword'].replace(' ','%20')}").content)
+        #print(searches[])
+        for search in searches['results']:
+            results = search['id'].strip() 
+            #print(results)
+            items = json.loads(rq.get(f"https://api.mercadolibre.com/items/{results}").content)
+            #print(results, items['catalog_product_id'])
+            item_id = items['catalog_product_id']
+            if not item_id == None: 
+                reviews = json.loads(rq.get(f"https://api.mercadolibre.com/reviews/item/{item_id}").content)
+                #print(rewis)
+                rating_avg = reviews['rating_average']
+                if rating_avg > 0.0:
+                    
+                    total = reviews['paging']['total']
+
+                    if rating_avg >  4.4 and total > 20:
+                        aux = 'Si'
+                    else:
+                        aux = 'No'
+                    print(total, rating_avg ,aux)
+
+                    for review in reviews['reviews']:
+                        #print(review['content']) 
+                        mysql_insertArticulos(item_id,items['title'],items['price'],reviews['rating_average'],total,aux)
+                        mysql_insertReview(review['id'],review['title'],review['content'],reviews['rating_average'],review['date_created'],item_id)
+
 if __name__=="__main__":
     get_tendencias()
--- main.sql
+++ main.sql
@@ -1,11 +1,17 @@
-create table articulos(
+drop database if exists mineria;
+
+create database if not exists mineria;
+
+create table mineria.articulos(
     id varchar(15) primary key,
     nombre_categoria varchar(80),
     precio_avg varchar(80),
-    rating_avg decimal(6,2)
-)
+    rating_avg decimal(6,2),
+    num_reviews int(3),
+    fut_tend varchar(3)
+);
 
-create table reviews(
+create table mineria.reviews(
     id varchar(10) primary key,
     titulo varchar(50),
     comentario varchar(500),
@@ -14,4 +20,5 @@ create table reviews(
     id_articulo varchar(15),
     CONSTRAINT FK_articulo FOREIGN KEY (id_articulo)
     REFERENCES articulos(id) 
-)
+);
+
